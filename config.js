2b5d0ee23c8b3b3c21731e689817831d23bdb861
Alex Beavil <alex@aeroniemi.com>
2021-01-29 16:02:47 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    mapboxToken: "pk.eyJ1IjoiYWVyb25pZW1pIiwiYSI6ImNpdWJjeHY5djAwMGUyeW80ZTR4ZjgwN3MifQ.PkPF6NIAFU3fOZgDTpCHgA"
+  mapboxToken:
+    "pk.eyJ1IjoiYWVyb25pZW1pIiwiYSI6ImNpdWJjeHY5djAwMGUyeW80ZTR4ZjgwN3MifQ.PkPF6NIAFU3fOZgDTpCHgA",
   port: 8080,
   maximumDatafeedAge: 60, // maximum datafeed age in seconds
   movingAveragePeriod: 20, // how many datapoints to account for when making moving averages
