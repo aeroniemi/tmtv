63e13ead0734857fbba46370eba8784c9a6d80a7
Alex Beavil <alex@aeroniemi.com>
2021-01-31 14:19:53 +0000

    fixed attribution text

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   mapboxToken:
     "pk.eyJ1IjoiYWVyb25pZW1pIiwiYSI6ImNpdWJjeHY5djAwMGUyeW80ZTR4ZjgwN3MifQ.PkPF6NIAFU3fOZgDTpCHgA",
+  mapId: "aeroniemi/ckjyvdfe01dnc17ppgsl0riga",
   port: 8080,
   maximumDatafeedAge: 60, // maximum datafeed age in seconds
   movingAveragePeriod: 20, // how many datapoints to account for when making moving averages
